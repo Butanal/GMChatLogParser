7c1c1a8c6fa33c38280454b0e5e4ffebaa0d9caa
Benjamin <bsepe@monaco.mc>
2019-03-15 14:34:16 +0100

    Init: first project commit

---
+++ examples/logprint.py
@@ -0,0 +1,12 @@
+import gmlogparser
+from datetime import datetime
+
+def format_date(timestamp):
+    date = datetime.fromtimestamp(timestamp)
+    return date.strftime("%H:%M:%S")
+
+logparser = gmlogparser.GMLogParser("/path/to/log/folder")
+
+logs = logparser.getChatLogs(startdate=1052479002)
+for log in logs:
+    print("[{}] {} : \"{}\"".format(format_date(log["timestamp"]), log["nick"], log["text"]))
---
+++ gmlogparser.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import codecs
+from datetime import datetime
+
+
+class GMLogParser:
+    def __init__(self, logdir):
+        if not logdir.endswith("/"):
+            logdir += "/"
+        files = os.listdir(logdir)
+        logs_exist = False
+        loglist = []
+        for file in files:
+            if file.endswith(".log"):
+                logs_exist = True
+                loglist.append((file, os.path.getmtime(logdir + file)))
+        if not logs_exist:
+            raise ValueError("Provided directory is not a log folder.")
+        self.loglist = sorted(loglist, key=lambda t: t[1])
+        self.logdir = logdir
+        self.pattern = re.compile(
+            '(\d{2}/\d{2}/\d{4}).*(\d{2}:\d{2}:\d{2}): "(.*)<\d*><(STEAM_\d:\d:\d*)><?([a-zA-Z]*)>?"? say "(.*)"')
+
+    def findChat(self, line):
+        results = re.search(self.pattern, line)
+        if results:
+            data = results.groups()
+            return {
+                "timestamp": int(datetime.strptime(data[0] + " " + data[1], "%m/%d/%Y %H:%M:%S").timestamp()),
+                "nick": data[2],
+                "steamid": data[3],
+                "teamchat": data[4] == "Team",
+                "text": data[5]
+            }
+
+    def findFileRange(self, startdate, enddate):
+        startindex = -1
+        endindex = -1
+
+        for i, file in enumerate(self.loglist):
+            if startindex == -1 and file[1] > startdate:
+                startindex = i
+                if not enddate:
+                    return startindex, len(self.loglist) - 1
+            elif endindex == -1 and enddate and file[1] > enddate:
+                return startindex, i
+
+    def getChatLogs(self, startdate, enddate=None, steamids=None):
+        daterange = self.findFileRange(startdate, enddate)
+        if not daterange:
+            return []
+        output = []
+
+        for i in range(daterange[0], daterange[1] + 1):
+            file = self.loglist[i][0]
+            if file.endswith(".log"):
+                with codecs.open(self.logdir + file, "r", encoding='utf-8', errors='ignore') as f:
+                    for line in f:
+                        result = self.findChat(line)
+                        if result and (not steamids or result["steamid"] in steamids):
+                            output.append(result)
+
+        return output
