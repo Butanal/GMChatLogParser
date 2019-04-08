9fe7a6a277f91e60b2413ff3f54b736a2ad6e623
Butanal <benji.sepe@gmail.com>
2019-04-08 23:04:21 +0200

    Explain structure of log dict

--- example_logprint.py
+++ example_logprint.py
@@ -10,3 +10,13 @@ logparser = gmlogparser.GMLogParser("/path/to/log/folder")
 logs = logparser.getChatLogs(startdate=1052479002, steamids=("STEAM_0:0:0"))
 for log in logs:
     print("[{}] {} : \"{}\"".format(format_date(log["timestamp"]), log["nick"], log["text"]))
+    
+    """ log contains :
+    {
+        "timestamp" : unix timestamp, seconds :,
+        "nick": username,
+        "steamid": SteamID,
+        "teamchat": [bool] whether message was in teamchat (true), else global chat (false),
+        "text": the message
+    }
+    """
